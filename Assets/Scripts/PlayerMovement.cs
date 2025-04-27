89518f9560be63008c34754c1999331cb8c90266
DDulla <yaperdilacabeza@outlook.com>
2025-04-26 22:17:55 -0500

    Now FootSteps

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,14 +7,33 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 moveInput;
     public Rigidbody rb;
 
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        moveInput = context.ReadValue<Vector2>();
-    }
+    [SerializeField] private AudioSource footstepSource;
 
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(moveInput.x, 0, moveInput.y) * moveSpeed;
         rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
+
+        HandleFootstepAudio();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void HandleFootstepAudio()
+    {
+        bool isMoving = rb.velocity.magnitude > 0.1f;
+
+        if (isMoving && !footstepSource.isPlaying) 
+        {
+            footstepSource.loop = true;
+            footstepSource.Play();
+        }
+        else if (!isMoving && footstepSource.isPlaying) 
+        {
+            footstepSource.Stop();
+        }
     }
 }
